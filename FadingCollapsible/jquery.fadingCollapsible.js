934603df8db0bbc2d9c4a98ace74bfaaa450c26d
danielcovill <daniel.covill@gmail.com>
2013-05-16 18:10:52 -0600

    Address rendering issues in older versions of IE

--- FadingCollapsible/jquery.fadingCollapsible.js
+++ FadingCollapsible/jquery.fadingCollapsible.js
@@ -20,33 +20,76 @@
 
 (function( $ ) {
 	$.fn.fadingCollapsible = function(method) {
-    if ( methods[method] ) {
-      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-    } else if ( typeof method === 'object' || ! method ) {
-      return methods.init.apply( this, arguments );
-    } else {
-      $.error( 'Method ' +  method + ' does not exist on jQuery.fadingCollapsible' );
-    }
+		if ( methods[method] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' + method + ' does not exist on jQuery.fadingCollapsible' );
+		}
 	};
 
-	function rgbaToRGB(input) {
-		//assume white background, if you want this to work on other backgrounds
-		//you'll need to modify the 255 added to each of r, g, b, and a below to 
-		//be the rgb values of the color behind your current color. (As opposed
-		//to the 255 I'm adding to each of r, g, and b)
+	function hexToRGB(input) {
+		var result;
+		if(input.substr(0,1) !== '#') {
+			throw "Error converting hex to rgb, bad format";
+		}
+		if(input.length === 4) {
+			var r = parseInt(input.substring(1,2) + input.substring(1,2), 16);
+			var g = parseInt(input.substring(2,3) + input.substring(2,3),16);
+			var b = parseInt(input.substring(3,4) + input.substring(3,4),16);
+			result = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+		} else if (input.length === 7) {
+			var r = parseInt(input.substring(1,3),16);
+			var g = parseInt(input.substring(3,5),16);
+			var b = parseInt(input.substring(5,7),16);
+			result = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+		} else {
+			throw "Error converting hex to rgb, bad format";
+		}
+		return result;
+	}
+
+	function rgbToHex(input) {
+		var parts = input.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+
+		delete(parts[0]);
+		for (var i = 1; i <= 3; ++i) {
+			parts[i] = parseInt(parts[i]).toString(16);
+			if (parts[i].length == 1) parts[i] = '0' + parts[i];
+		} 
+		return parts.join('');
+	}
+
+	function colorToRGB(input) {
+		/*if the incoming color has transparency this method will assume there's a white underlay, 
+		 * if you want this to work on other backgrounds you'll need to modify the 255 added to 
+		 * each of r, g, b, and a below to be the rgb values of the color behind your current 
+		 * color. (As opposed to the 255 I'm adding to each of r, g, and b). If THAT one is also
+		 * transparent, you'll need to take that into account in your calculations as well.
+		 * http://stackoverflow.com/questions/2049230/convert-rgba-color-to-rgb
+		*/
+		if(input.substr(0,1) === '#') {
+			return hexToRGB(input);
+		}
+
+		if(input === 'transparent') {
+			return 'rgb(255,255,255)';
+		}
+
 		if(input.substr(0,4) === 'rgb(') {
 			return input;
 		}
-		var re = new RegExp(/[^\d]+(\d{1,3}), (\d{1,3}), (\d{1,3}), ([^\)]*)\)/g);
+		var re = new RegExp(/[^\d]+(\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*([^\)]*)\)/g);
 
-		var r = input.replace(re, '$1')/255;
-		var g = input.replace(re, '$2')/255;
-		var b = input.replace(re, '$3')/255;
-
+		var r = input.replace(re, '$1');
+		var g = input.replace(re, '$2');
+		var b = input.replace(re, '$3');
 		var a = input.replace(re, '$4');
-		r = (255 + (((1-a) * r) + (a * r))) * 255;
-		g = (255 + (((1-a) * g) + (a * g))) * 255;
-		b = (255 + (((1-a) * b) + (a * b))) * 255;
+
+		r = Math.round(((1 - a) * 255) + (a * r));
+		g = Math.round(((1 - a) * 255) + (a * g));
+		b = Math.round(((1 - a) * 255) + (a * b));
 
 		input = 'rgb(' + r + ', ' + g + ', ' + b + ')';
 		return input;
@@ -59,7 +102,6 @@
 						data = $this.data('fadingCollapsible');
 	
 				if(!data) {
-	
 					var originalSettings = {
 						'height': $this.height(),
 						'overflow': $this.css('overflow'),
@@ -86,7 +128,7 @@
 						'more-button-text': 'more',
 						'less-button-text': 'less',
 						'button-class': 'fauxLink',
-						'background-color-rgb': rgbaToRGB(originalSettings['background-color']),
+						'background-color-rgb': colorToRGB(originalSettings['background-color']),
 						'opened': false
 					};
 
@@ -97,7 +139,7 @@
 						$this.css({
 							'position': 'relative', 
 							'background-color': settings["background-color-rgb"],
-							'padding-bottom': parseInt(originalSettings['padding-bottom']) + 35 + 'px',
+							'padding-bottom': parseInt(originalSettings['padding-bottom']) + 35 + 'px'
 						});
 					} else {
 						$this.css({
@@ -127,24 +169,41 @@
 						pluginZIndex = parseInt($this.css("z-index")) + 1
 					}
 		
+					var borderLeftWidth = parseInt($this.css("border-left-width"));
+					if(isNaN(borderLeftWidth)) {
+						borderLeftWidth = 0;
+					}
+					var borderRightWidth = parseInt($this.css("border-right-width"));
+					if(isNaN(borderRightWidth)) {
+						borderRightWidth = 0;
+					}
 					//create the fade div
 					var fadeDiv = $('<div />', { 
-						class:'textFade',
+						'class':'textFade',
 						css: {
 							'position': 'absolute',
 							'bottom': '30px',
 							'left': '0',
 							'z-index': pluginZIndex,
-							'width': $this.outerWidth() - parseInt($this.css("border-left-width")) - parseInt($this.css("border-right-width")) + 'px',
+							'width': $this.outerWidth() - borderLeftWidth - borderRightWidth + 'px',
 							'height': '50px',
 							'background-image': background
 						}
 					});
+					if($.browser.msie && $.browser.version < 10) {
+						var stupidIEColor = rgbToHex(settings["background-color-rgb"]);
+						fadeDiv.css({ 
+							'zoom': '1',
+							'background': 'none',
+							'-ms-filter': 'progid:DXImageTransform.Microsoft.gradient(startColorstr=#00' + stupidIEColor + ',endColorstr=#ff' + stupidIEColor + ')',
+							'filter': 'progid:DXImageTransform.Microsoft.gradient(startColorstr=#00' + stupidIEColor + ',endColorstr=#ff' + stupidIEColor + ')'
+						});
+					}
 					if(!settings['opened']) { fadeDiv.appendTo($this); }		
 
 					//add the more/less button and related items
 					var controlDiv = $('<div />', {
-						class:'collapseButton',
+						'class':'collapseButton',
 						css: {
 							'position': 'absolute',
 							'bottom': '0',
@@ -152,10 +211,10 @@
 							'padding-left': $this.css('padding-left'),
 							'z-index': pluginZIndex,
 							'height': '30px',
-							'width': $this.outerWidth() - parseInt($this.css("border-left-width")) - parseInt($this.css("border-right-width")) - parseInt($this.css('padding-left')) + 'px',
+							'width': $this.outerWidth() - borderLeftWidth - borderRightWidth - parseInt($this.css('padding-left')) + 'px',
 							'line-height': '20px',
 							'cursor': 'pointer',
-							'background-color': settings["background-color-rgb"],
+							'background-color': settings["background-color-rgb"]
 						},
 						click: function() {
 							methods.toggle($(this.parentElement));
@@ -180,7 +239,7 @@
 							'margin-left': '5px',
 							'vertical-align': 'top'
 						},
-						class: settings['button-class']
+						'class': settings['button-class']
 					}).appendTo(controlDiv);
 	
 					var controlIcon = $('<img />', {
@@ -225,9 +284,9 @@
 		},
 		expand : function(obj) {
 			var fadingCollapsible = obj.data('fadingCollapsible');
+			obj.css('overflow', fadingCollapsible.originalSettings['overflow']);
 			obj.animate({
 				'height':fadingCollapsible.originalSettings['height'],
-				'overflow':fadingCollapsible.originalSettings['overflow'],
 				'padding-bottom':parseInt(fadingCollapsible.originalSettings['padding-bottom']) + 35 + 'px'
 			}, 500, function() {
 				fadingCollapsible.fadeDiv.detach();
